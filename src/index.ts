4fbf85af526a11585c598b69dafc619cdccc8122
mojo-jojo-lol <110245542+mojo-jojo-lol@users.noreply.github.com>
2023-05-06 19:28:12 +0200

    ddd

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@
  */
 const main = (value: string) => {
   const passedValue = value;
+  const c = "2";
+  const d = "0";
+  const e = "1";
+  const f = "1";
+  const g = "1";
   return passedValue;
 }
 
